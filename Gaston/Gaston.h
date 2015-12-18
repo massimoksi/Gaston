d51f776f3f49c8d0ef2e3ba8a38cfd9ac2b3a920
Massimo Peri <massimoperi@gmail.com>
2015-12-18 16:00:32 +0100

    Initial Commit

---
+++ Gaston/Gaston.h
@@ -0,0 +1,19 @@
+//
+//  Gaston.h
+//  Gaston
+//
+//  Created by Massimo Peri on 18/12/15.
+//  Copyright © 2015 Massimo Peri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Gaston.
+FOUNDATION_EXPORT double GastonVersionNumber;
+
+//! Project version string for Gaston.
+FOUNDATION_EXPORT const unsigned char GastonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gaston/PublicHeader.h>
+
+
